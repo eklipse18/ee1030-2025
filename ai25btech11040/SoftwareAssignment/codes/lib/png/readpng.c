dd9a4b165ea0506acbd36ab07d849e740194ca29
Vivaan Parashar <parashar.xtra@gmail.com>
2025-11-06 16:14:02 +0530

    holy shit 8bit and 16bit depth grayscale no alpha readpng works

--- ai25btech11040/SoftwareAssignment/codes/lib/png/readpng.c
+++ ai25btech11040/SoftwareAssignment/codes/lib/png/readpng.c
@@ -50,6 +50,19 @@ unsigned long crc(unsigned char *buf, int len) {
   return update_crc(0xffffffffL, buf, len) ^ 0xffffffffL;
 }
 
+int paeth(int a, int b, int c) {
+  int p = a + b - c;
+  int pa = abs(p - a);
+  int pb = abs(p - b);
+  int pc = abs(p - c);
+  if (pa <= pb && pa <= pc)
+    return a;
+  else if (pb <= pc)
+    return b;
+  else
+    return c;
+}
+
 int **readpng(const char *filename, int ihdr_[7]) {
   FILE *file = fopen(filename, "rb");
   if (file == NULL) {
@@ -72,6 +85,16 @@ int **readpng(const char *filename, int ihdr_[7]) {
   int **array = NULL;
   Bytef *decompressed = NULL;
   int **unfiltered = NULL;
+  
+  z_stream strm;
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  strm.avail_in = 0;
+  strm.next_in = Z_NULL;
+  int ret = inflateInit(&strm);
+  if (ret != Z_OK)
+    goto malformed;
 
   while (i < filelen && !feof(file)) {
     // Make sure its a PNG image
@@ -103,20 +126,10 @@ int **readpng(const char *filename, int ihdr_[7]) {
     fread(chunkData, 1, chunkLength, file);
     unsigned char crc[4];
     fread(crc, 1, 4, file);
-    printf("%s %u\n", chunkType, chunkLength); // DEBUG
+    // printf("%s %u\n", chunkType, chunkLength); // DEBUG
     // TODO: Implement CRC check
 
     /* PROCESS CHUNK DATA */
-
-    z_stream strm;
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    int ret = inflateInit(&strm);
-    if (ret != Z_OK) goto malformed;
-
     if (strcmp((const char *)chunkType, "IHDR") == 0) {
       if (chunkLength != 13)
         goto malformed; // IHDR chunk must be 13 bytes long
@@ -132,12 +145,13 @@ int **readpng(const char *filename, int ihdr_[7]) {
         ihdr[1] = (ihdr[1] << 8) | chunkData[j];
       }
 
-      ihdr[2] = chunkData[8];  // bit depth
-      ihdr[3] = chunkData[9];  // color type
-      ihdr[4] = chunkData[10]; // compression method
-      ihdr[5] = chunkData[11]; // filter method
-      ihdr[6] = chunkData[12]; // interlace method
-      ihdr_ = ihdr;
+      ihdr_[2] = ihdr[2] = chunkData[8];  // bit depth
+      ihdr_[3] = ihdr[3] = chunkData[9];  // color type
+      ihdr_[4] = ihdr[4] = chunkData[10]; // compression method
+      ihdr_[5] = ihdr[5] = chunkData[11]; // filter method
+      ihdr_[6] = ihdr[6] = chunkData[12]; // interlace method
+      ihdr_[0] = ihdr[0];
+      ihdr_[1] = ihdr[1];
 
       printf(
           "Width: %d, Height: %d, Bit depth: %d, Color type: %d, Compression "
@@ -145,8 +159,7 @@ int **readpng(const char *filename, int ihdr_[7]) {
           ihdr[0], ihdr[1], ihdr[2], ihdr[3], ihdr[4], ihdr[5],
           ihdr[6]); // DEBUG
       if (ihdr[3] != 0 || ihdr[6] != 0)
-        goto notimplemented; // TODO:
-      // Implement for all types of PNGs
+        goto notimplemented; // TODO: Implement for all types of PNGs
 
       if (ihdr[4] != 0 || ihdr[5] != 0)
         goto malformed;
@@ -158,27 +171,24 @@ int **readpng(const char *filename, int ihdr_[7]) {
       // Image data chunk
       if (chunkLength == 0)
         goto malformed;
-      if (ihdr[3] == 0) {
-        // Grayscale image, no alpha
-        if (decompressed == NULL) {
-          decompressed =
-              malloc(ihdr[1] * (ihdr[0] + 1) * ihdr[2]/8 * sizeof(Bytef));
-        } // TODO: Implement ZLIB decompression
-        strm.next_out = decompressed;
-        strm.avail_out = ihdr[1] * (ihdr[0] + 1) * sizeof(Bytef);
-        strm.next_in = chunkData;
-        strm.avail_in = chunkLength;
-        ret = inflate(&strm, Z_NO_FLUSH);
-        switch (ret) {
-        case Z_NEED_DICT:
-        case Z_DATA_ERROR:
-        case Z_MEM_ERROR: {
-          inflateEnd(&strm);
-          goto malformed; // Error during decompression
-          break;
-        }
-        case Z_STREAM_END: inflateEnd(&strm);
-        }
+      if (decompressed == NULL) {
+        decompressed = malloc(ihdr[1] * (ihdr[0] + 1) * ihdr[2] / 8);
+      } // TODO: Implement ZLIB decompression
+      strm.next_out = decompressed;
+      strm.avail_out = ihdr[1] * (ihdr[0] + 1) * ihdr[2] / 8;
+      strm.next_in = chunkData;
+      strm.avail_in = chunkLength;
+      ret = inflate(&strm, Z_NO_FLUSH);
+      switch (ret) {
+      case Z_NEED_DICT:
+      case Z_DATA_ERROR:
+      case Z_MEM_ERROR: {
+        inflateEnd(&strm);
+        goto malformed; // Error during decompression
+        break;
+      }
+      case Z_STREAM_END:
+        inflateEnd(&strm);
       }
 
       // TODO Read IDAT for all other types of PNGs
@@ -227,49 +237,93 @@ int **readpng(const char *filename, int ihdr_[7]) {
     i += 4 + 4 + chunkLength + 4;
     free(chunkData);
   }
-  // DEBUG: Printing decompressed
+
   int bd = ihdr[2]; // bit depth
+  int bpp = (bd + 7) / 8; // bytes per pixel, for grayscale images
+  int w = (ihdr[0] * bd + 7) / 8;
+  // Unfiltering process
+  unfiltered = malloc(ihdr[1] * sizeof(int *));
+  for (int j = 0; j < ihdr[1]; j++) {
+    unfiltered[j] = calloc(w, sizeof(int));
+  }
+
   for (int i = 0; i < ihdr[1]; i++) {
-    int ftype = decompressed[i * (ihdr[0] + 1)];
-    printf("Scanline %d filter type: %d\n", i, ftype);
-    for (int j = 1; j <= ihdr[0]; j++) {
-      int k = 0;
-      for (int z = 0; z < bd; z++) {
-        k = (k << 8) | decompressed[i * (ihdr[0] + 1) + bd*j + z];
-      }
+    int ftype = decompressed[i * (w + 1)]; // filter type
+    // printf("Scanline %d filter type: %d\n", i, ftype); // DEBUG
+    for (int j = 1; j <= w; j++) {
+      int k = decompressed[i * (w + 1) + j];
       switch (ftype) {
       case 0:
         unfiltered[i][j - 1] = k;
         break;
       case 1:
         // Sub filter
-        if (j != 1)
-          unfiltered[i][j] = (k + decompressed[i * (ihdr[0] + 1) + j - 1]) % 256;
+        unfiltered[i][j - 1] = (k + (j - 1 - bpp < 0 ? 0 : unfiltered[i][j - 1 - bpp])) % 256;
         break;
       case 2:
         // Up filter
-        if (i == 0)
-          k = k;
-        else
-          k = (k + decompressed[(i - 1) * (ihdr[0] + 1) + j]) % 256;
+        unfiltered[i][j - 1] = (k + (i == 0 ? 0 : unfiltered[i - 1][j - 1])) % 256;
         break;
       case 3:
         // Average filter
         {
           int left = 0, up = 0;
-          if (j != 1)
-            left = decompressed[i * (ihdr[0] + 1) + j - 1];
-          if (i != 0)
-            up = decompressed[(i - 1) * (ihdr[0] + 1) + j];
-          k = (k + ((left + up) / 2)) % 256;
+          if (j - 1 - bpp >= 0)
+            left = unfiltered[i][j - 1 - bpp];
+          if (i > 0)
+            up = unfiltered[i - 1][j - 1];
+          unfiltered[i][j - 1] = (k + ((left + up) / 2)) % 256;
+          break;
         }
+      case 4:
+        // Paeth filter
+        {
+          int left = 0, up = 0, up_left = 0;
+          if (j - 1 - bpp >= 0)
+            left = unfiltered[i][j - 1 - bpp];
+          if (i > 0)
+            up = unfiltered[i - 1][j - 1];
+          if (i > 0 && j - 1 - bpp >= 0)
+            up_left = unfiltered[i - 1][j - 1 - bpp];
+          unfiltered[i][j - 1] = (k + paeth(left, up, up_left)) % 256;
+          break;
+        }
+      default:
+        goto malformed; // Invalid filter type
       }
     }
   }
 
+  // DEBUG: Print unfiltered data
+  // printf("Unfiltered data:\n");
+  // for (int i = 0; i < ihdr[1]; i++) {
+  //   for (int j = 0; j < (ihdr[0] * bd + 7) / 8; j++) {
+  //     printf("%4d", unfiltered[i][j]);
+  //     // printf("\x1B[48;5;%dm  \x1B[0m", 232 + (unfiltered[i][j])*23/255);
+  //   }
+  //   printf("\n");
+  // }
+
   array = malloc(ihdr[1] * sizeof(int *));
   for (int j = 0; j < ihdr[1]; j++) {
     array[j] = calloc(ihdr[0], sizeof(int));
+  }
+
+  // Convert unfiltered data to final pixel values
+  for (int i = 0; i < ihdr[1]; i++) {
+    for (int j = 0; j < ihdr[0]; j++) {
+      for (int z = 0; z < bd / 8; z++) {
+        array[i][j] = (array[i][j] << 8) | unfiltered[i][j * (bd / 8) + z];
+      }
+    }
+  }
+  printf("Array data:\n");
+  for (int i = 0; i < ihdr[1]; i++) {
+    for (int j = 0; j < ihdr[0]; j++) {
+      printf("\x1B[48;5;%dm  \x1B[0m",
+             232 + (array[i][j]) * 23 / ((1 << bd) - 1));
+    }
+    printf("\n");
   }
 
   /* Section for various error labels */
